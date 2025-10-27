b1669478a5b962ab267caf66d28915e4004524fb
Jose Mario Nuñez Justiniano <a2024111939@estudiantes.upsa.edu.bo>
2025-10-27 19:37:01 -0400

    Commit add DTOs, Services and Data

---
+++ SegundopApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundopApi.Models;
+
+namespace SegundopApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Product> Products => Set<Product>();
+}
