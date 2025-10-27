ba4c951cd213de49d312dbb2edb8188730553a4c
Jose Mario Nuñez Justiniano <a2024111939@estudiantes.upsa.edu.bo>
2025-10-27 19:33:15 -0400

    Commit add models

---
+++ SegundopApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace SegundopApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public int UserId { get; set; }
+    public User? User { get; set; }
+}
